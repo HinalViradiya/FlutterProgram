9bba5431efc9d870828b5346d58b8ecd45a8f097
HinalViradiya <hinalviradiya452@gmail.com>
2022-07-14 16:46:00 +0530

    14/07/2022 uploading Images

--- tabbar_view_ex/lib/tabs/bottom/music.dart
+++ tabbar_view_ex/lib/tabs/bottom/music.dart
@@ -1,14 +1,33 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
 
 class MusicTab extends StatelessWidget {
   const MusicTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      child:const Center(child: Text("Music Tab",style: TextStyle(fontSize: 24),))
+    
+    return Container(
+      // child:const Center(child: Text("Music Tab",style: TextStyle(fontSize: 24),))
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children:  <Widget> [
+               const  Padding(
+                  padding:  EdgeInsets.all(8.0),
+                  child:  Text(
+                    "New Realese",
+                    style: TextStyle(color: Colors.black87,fontSize: 18),
+                  ),
+                ),
+               const Spacer(),
+                TextButton(onPressed: (){}, child:const  Text("MORE",style: TextStyle(fontSize: 15),),)
+              ],
+            ),            
+          ],
+        ),
+      ),
     );
   }
 }
